6f488347bda04b760ba88144f02cdb25ff96db44
Doodle6660 <ziyadzidane69@gmail.com>
2021-01-29 21:48:33 +0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,138 +1,30 @@
 <template>
-    <div class="hello">
-        <h1>{{ msg }}</h1>
-        <p>
-            For a guide and recipes on how to configure / customize this
-            project,<br />
-            check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-                >vue-cli documentation</a
-            >.
-        </p>
-        <h3>Installed CLI Plugins</h3>
-        <ul>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-                    target="_blank"
-                    rel="noopener"
-                    >babel</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-                    target="_blank"
-                    rel="noopener"
-                    >router</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-                    target="_blank"
-                    rel="noopener"
-                    >eslint</a
-                >
-            </li>
-        </ul>
-        <h3>Essential Links</h3>
-        <ul>
-            <li>
-                <a href="https://vuejs.org" target="_blank" rel="noopener"
-                    >Core Docs</a
-                >
-            </li>
-            <li>
-                <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-                    >Forum</a
-                >
-            </li>
-            <li>
-                <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-                    >Community Chat</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://twitter.com/vuejs"
-                    target="_blank"
-                    rel="noopener"
-                    >Twitter</a
-                >
-            </li>
-            <li>
-                <a href="https://news.vuejs.org" target="_blank" rel="noopener"
-                    >News</a
-                >
-            </li>
-        </ul>
-        <h3>Ecosystem</h3>
-        <ul>
-            <li>
-                <a
-                    href="https://router.vuejs.org"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-router</a
-                >
-            </li>
-            <li>
-                <a href="https://vuex.vuejs.org" target="_blank" rel="noopener"
-                    >vuex</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/vue-devtools#vue-devtools"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-devtools</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://vue-loader.vuejs.org"
-                    target="_blank"
-                    rel="noopener"
-                    >vue-loader</a
-                >
-            </li>
-            <li>
-                <a
-                    href="https://github.com/vuejs/awesome-vue"
-                    target="_blank"
-                    rel="noopener"
-                    >awesome-vue</a
-                >
-            </li>
-        </ul>
+    <div class="h-screen bg-gradient-to-r from-black to-primary text-white">
+        <div
+            class="flex flex-col max-w-xl h-full items-start justify-center ml-6 xl:ml-12 font-primary"
+        >
+            <div
+                class="text-6xl xl:text-8xl leading-snug xl:leading-snug font-bold"
+            >
+                <p>Hello.</p>
+                <p>Im Ziyad</p>
+            </div>
+            <div class="text-3xl xl:text-5xl mt-8">
+                <p>Front-End & UI/UX Designer</p>
+            </div>
+            <div class="text-md xl:text-2xl mt-20 font-secondary">
+                <p>
+                    Hi!
+                    <span class="font-semibold">I'm an 18-year-old maker</span>
+                    who enjoys building simple & clean websites that are easy to
+                    use and beautiful to look at.
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
-<script>
-export default {
-    name: "HelloWorld",
-    props: {
-        msg: String,
-    },
-}
-</script>
+<script></script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-    margin: 40px 0 0;
-}
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-a {
-    color: #42b983;
-}
-</style>
+<style scoped></style>
