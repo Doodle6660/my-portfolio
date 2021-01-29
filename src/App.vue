0fdb01537515b99e797280931ad4d8391b176e72
Doodle6660 <ziyadzidane69@gmail.com>
2021-01-29 21:48:09 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,25 +6,9 @@
     <router-view />
 </template>
 
+export default { components: { }, data () { return { } }, methods: { } }
+
 <style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-    color: #42b983;
-}
+/* import fonts */
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Quicksand:wght@300;400;500;600;700&display=swap");
 </style>
