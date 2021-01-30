781b197dbe301123081729c1e6ece9d4a2cfce5a
Doodle6660 <ziyadzidane69@gmail.com>
2021-01-30 20:27:34 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="home">
         <app-home></app-home>
+        <app-links></app-links>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue"
+import QuickLinks from "@/components/QuickLinks.vue"
 
 export default {
     components: {
         "app-home": HelloWorld,
+        "app-links": QuickLinks,
     },
 }
 </script>
