f523ec545e758e92a14a9b85258e49ba3404d73c
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-01 11:38:30 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
     <div class="home">
         <app-home></app-home>
         <app-links></app-links>
+        <app-bio></app-bio>
     </div>
 </template>
 
 <script>
 import HelloWorld from "@/components/HelloWorld.vue"
 import QuickLinks from "@/components/QuickLinks.vue"
+import Biography from "@/components/Biography.vue"
 
 export default {
     components: {
         "app-home": HelloWorld,
         "app-links": QuickLinks,
+        "app-bio": Biography,
     },
 }
 </script>
