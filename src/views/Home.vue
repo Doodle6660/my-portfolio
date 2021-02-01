c7c59dc6132e35cac8f54865f8438dbac5795c64
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-01 22:27:21 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
         <app-home></app-home>
         <app-links></app-links>
         <app-bio></app-bio>
+        <app-project></app-project>
     </div>
 </template>
 
@@ -10,12 +11,14 @@
 import HelloWorld from "@/components/HelloWorld.vue"
 import QuickLinks from "@/components/QuickLinks.vue"
 import Biography from "@/components/Biography.vue"
+import Project from "@/components/Project.vue"
 
 export default {
     components: {
         "app-home": HelloWorld,
         "app-links": QuickLinks,
         "app-bio": Biography,
+        "app-project": Project,
     },
 }
 </script>
