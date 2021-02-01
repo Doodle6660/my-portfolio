d99292b6d6e69ccf04a527170c62b8d1be760b8c
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-01 11:37:55 +0700

    Update Biography.vue

--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -1,5 +1,156 @@
 <template>
-    <div></div>
+    <div class="flex h-screen w-full font-secondary py-10 justify-center">
+        <div class="bg-red-500 w-11/12 px-6 py-8 rounded-md">
+            <p
+                class="w-3/4 xl:w-1/5 bg-white text-red-500 font-semibold text-2xl md:text-3xl px-2 py-2 rounded-sm mb-10"
+            >
+                Biography .
+            </p>
+            <div class="flex flex-row w-full">
+                <div class="flex flex-col w-1/2 md:w-1/4 text-white gap-6">
+                    <p class="text-2xl md:text-4xl w-1/6 font-primary">
+                        PERSONAL INFO
+                    </p>
+                    <div class="flex flex-row items-center space-x-2">
+                        <svg
+                            id="light"
+                            enable-background="new 0 0 24 24"
+                            viewBox="0 0 24 24"
+                            width="512"
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-4 xl:w-8 h-4 xl:h-8 fill-current text-white"
+                        >
+                            <g>
+                                <path
+                                    d="m21.5 21h-19c-1.378 0-2.5-1.122-2.5-2.5v-13c0-1.378 1.122-2.5 2.5-2.5h19c1.378 0 2.5 1.122 2.5 2.5v13c0 1.378-1.122 2.5-2.5 2.5zm-19-17c-.827 0-1.5.673-1.5 1.5v13c0 .827.673 1.5 1.5 1.5h19c.827 0 1.5-.673 1.5-1.5v-13c0-.827-.673-1.5-1.5-1.5z"
+                                />
+                            </g>
+                            <g>
+                                <path
+                                    d="m7.5 12c-1.378 0-2.5-1.122-2.5-2.5s1.122-2.5 2.5-2.5 2.5 1.122 2.5 2.5-1.122 2.5-2.5 2.5zm0-4c-.827 0-1.5.673-1.5 1.5s.673 1.5 1.5 1.5 1.5-.673 1.5-1.5-.673-1.5-1.5-1.5z"
+                                />
+                            </g>
+                            <g>
+                                <path
+                                    d="m11.5 17c-.276 0-.5-.224-.5-.5v-1c0-.827-.673-1.5-1.5-1.5h-4c-.827 0-1.5.673-1.5 1.5v1c0 .276-.224.5-.5.5s-.5-.224-.5-.5v-1c0-1.378 1.122-2.5 2.5-2.5h4c1.378 0 2.5 1.122 2.5 2.5v1c0 .276-.224.5-.5.5z"
+                                />
+                            </g>
+                            <g>
+                                <path
+                                    d="m20.5 9h-6c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h6c.276 0 .5.224.5.5s-.224.5-.5.5z"
+                                />
+                            </g>
+                            <g>
+                                <path
+                                    d="m20.5 13h-6c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h6c.276 0 .5.224.5.5s-.224.5-.5.5z"
+                                />
+                            </g>
+                            <g>
+                                <path
+                                    d="m20.5 17h-6c-.276 0-.5-.224-.5-.5s.224-.5.5-.5h6c.276 0 .5.224.5.5s-.224.5-.5.5z"
+                                />
+                            </g>
+                        </svg>
+                        <p>Ziyad F. Zidane</p>
+                    </div>
+                    <div class="flex flex-row items-center space-x-2">
+                        <svg
+                            version="1.1"
+                            id="Capa_1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                            x="0px"
+                            y="0px"
+                            viewBox="0 0 512 512"
+                            xml:space="preserve"
+                            class="w-4 md:w-8 h-4 md:h-8 fill-current text-white"
+                        >
+                            <g>
+                                <g>
+                                    <path
+                                        d="M467,76H45C20.137,76,0,96.262,0,121v270c0,24.885,20.285,45,45,45h422c24.655,0,45-20.03,45-45V121
+			C512,96.306,491.943,76,467,76z M460.698,106c-9.194,9.145-167.415,166.533-172.878,171.967c-8.5,8.5-19.8,13.18-31.82,13.18
+			s-23.32-4.681-31.848-13.208C220.478,274.284,64.003,118.634,51.302,106H460.698z M30,384.894V127.125L159.638,256.08L30,384.894z
+			M51.321,406l129.587-128.763l22.059,21.943c14.166,14.166,33,21.967,53.033,21.967c20.033,0,38.867-7.801,53.005-21.939
+			l22.087-21.971L460.679,406H51.321z M482,384.894L352.362,256.08L482,127.125V384.894z"
+                                    />
+                                </g>
+                            </g>
+                        </svg>
+                        <p>ziyadzidane69@gmail.com</p>
+                    </div>
+                    <div class="flex flex-row items-center space-x-2">
+                        <svg
+                            version="1.1"
+                            id="Capa_1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink"
+                            x="0px"
+                            y="0px"
+                            viewBox="0 0 384 384"
+                            xml:space="preserve"
+                            class="w-4 md:w-8 h-4 md:h-8 fill-current text-white"
+                        >
+                            <g>
+                                <g>
+                                    <path
+                                        d="M353.188,252.052c-23.51,0-46.594-3.677-68.469-10.906c-10.719-3.656-23.896-0.302-30.438,6.417l-43.177,32.594
+			c-50.073-26.729-80.917-57.563-107.281-107.26l31.635-42.052c8.219-8.208,11.167-20.198,7.635-31.448
+			c-7.26-21.99-10.948-45.063-10.948-68.583C132.146,13.823,118.323,0,101.333,0H30.813C13.823,0,0,13.823,0,30.813
+			C0,225.563,158.438,384,353.188,384c16.99,0,30.813-13.823,30.813-30.813v-70.323C384,265.875,370.177,252.052,353.188,252.052z"
+                                    />
+                                </g>
+                            </g>
+                        </svg>
+                        <p>+62 821-1785-5840</p>
+                    </div>
+                    <div class="flex flex-row items-center space-x-2">
+                        <svg
+                            id="Layer_1"
+                            enable-background="new 0 0 512 512"
+                            viewBox="0 0 512 512"
+                            width="512"
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-4 md:w-8 w-4 md:h-8 fill-current text-white"
+                        >
+                            <g>
+                                <path
+                                    d="m407.579 87.677c-31.073-53.624-86.265-86.385-147.64-87.637-2.62-.054-5.257-.054-7.878 0-61.374 1.252-116.566 34.013-147.64 87.637-31.762 54.812-32.631 120.652-2.325 176.123l126.963 232.387c.057.103.114.206.173.308 5.586 9.709 15.593 15.505 26.77 15.505 11.176 0 21.183-5.797 26.768-15.505.059-.102.116-.205.173-.308l126.963-232.387c30.304-55.471 29.435-121.311-2.327-176.123zm-151.579 144.323c-39.701 0-72-32.299-72-72s32.299-72 72-72 72 32.299 72 72-32.298 72-72 72z"
+                                />
+                            </g>
+                        </svg>
+                        <p>Bojongsoang, Bandung</p>
+                    </div>
+                </div>
+                <div
+                    class="flex flex-col w-1/2 md:w-3/4 text-white justify-between"
+                >
+                    <p class="text-2xl md:text-3xl font-semibold">
+                        Hi! I'm an 18-year-old maker who enjoys creating fun
+                        stuff.
+                    </p>
+                    <p>
+                        I am a Web and User Interface Designer based in Bandung,
+                        Indonesia, I love building simple & clean websites that
+                        are easy to use and beautiful to look at
+                    </p>
+                    <p>
+                        I’ve been a big fan of programming computers ever since
+                        I was a lil' booger. The process of creating something
+                        out of nothing has always been what I loved most about
+                        it.
+                    </p>
+                    <p>
+                        I love to draw. I don’t really drawing about anything in
+                        particular, just whatever’s bouncing around in my head
+                        or from anime which I just watched. You can check out
+                        mah art work if you're interested, and I'd love it if
+                        you did.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
