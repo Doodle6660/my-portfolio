e2fdf7cc839b145e4f724baf0565f4dc21f90f93
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-02 16:48:33 +0700

    Update Biography.vue

--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -1,15 +1,17 @@
 <template>
-    <div
-        class="flex h-screen w-full h-full font-secondary py-10 justify-center"
-    >
-        <div class="bg-moon1 w-11/12 h-full px-6 py-8 rounded-md">
+    <div class="flex h-screen w-full font-secondary py-10 justify-center">
+        <div
+            class="bg-moon1 w-11/12 h-full px-2 md:px-6 py-6 md:py-8 rounded-md"
+        >
             <p
-                class="w-3/4 xl:w-1/5 bg-white text-moon1 font-semibold text-2xl md:text-3xl px-2 py-2 rounded-sm mb-10"
+                class="w-3/4 xl:w-1/5 bg-white text-moon1 font-semibold text-2xl md:text-3xl px-2 py-2 rounded-sm mb-4 md:mb-10"
             >
                 Biography .
             </p>
             <div class="flex flex-row w-full h-5/6 px-4">
-                <div class="flex flex-col w-1/2 md:w-2/6 text-white gap-6">
+                <div
+                    class="flex flex-col w-1/2 md:w-2/6 text-white gap-6 text-xs md:text-base"
+                >
                     <p class="text-2xl md:text-4xl w-1/6 font-primary">
                         PERSONAL INFO
                     </p>
@@ -43,7 +45,7 @@
                             ></path>
                             <polyline points="22,6 12,13 2,6"></polyline>
                         </svg>
-                        <p>ziyadzidane69@gmail.com</p>
+                        <p class="truncate">ziyadzidane69@gmail.com</p>
                     </div>
                     <div class="flex flex-row items-center space-x-2">
                         <svg
@@ -74,28 +76,30 @@
                                 d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z"
                             />
                         </svg>
-                        <p>Bojongsoang, Bandung</p>
+                        <p>Bandung, Indonesia</p>
                     </div>
                 </div>
-                <div class="flex flex-col w-1/2 md:w-4/6 text-white gap-6">
-                    <p class="text-2xl md:text-3xl font-semibold mb-6">
+                <div
+                    class="flex flex-col w-1/2 md:w-4/6 text-white gap-3 md:gap-6 text-xs md:text-xl ml-3 md:ml-0"
+                >
+                    <p class="text-base md:text-3xl font-semibold mb-3 md:mb-6">
                         Hi! I'm an 18-year-old maker who enjoys creating fun
                         stuff.
                     </p>
-                    <p class="text-xl">
+                    <p>
                         I am a Web and User Interface Designer based in
                         <span class="font-semibold">Bandung, Indonesia,</span> I
                         love building simple & clean websites that are easy to
                         use and beautiful to look at
                     </p>
-                    <p class="text-xl">
+                    <p class="hidden md:block">
                         I’ve been a big fan of
                         <span class="font-semibold">programming</span> computers
                         ever since I was a lil' booger. The process of creating
                         something out of nothing has always been what I loved
                         most about it.
                     </p>
-                    <p class="text-xl">
+                    <p>
                         I love to draw. I don’t really drawing about anything in
                         particular, just whatever’s bouncing around in my head
                         or from anime which I just watched. You can check out
