5006896b9e54dda15b79c2fe82066481e02c34f6
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-02 16:48:25 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,22 @@ export default {
 <style>
 /* import fonts */
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Quicksand:wght@300;400;500;600;700&display=swap");
+
+::-webkit-scrollbar {
+    width: 0.375rem;
+    height: 6px;
+}
+
+::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px #000;
+}
+
+::-webkit-scrollbar-thumb {
+    @apply bg-moon1;
+    @apply rounded;
+}
+
+::-webkit-scrollbar-thumb:hover {
+    @apply bg-moon3;
+}
 </style>
