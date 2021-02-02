58030f5d458cb27ba46fd604f158df1db6f8356b
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-02 16:48:44 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
         <app-bio></app-bio>
         <app-project></app-project>
         <app-art></app-art>
+        <app-footer></app-footer>
     </div>
 </template>
 
@@ -14,6 +15,7 @@ import QuickLinks from "@/components/QuickLinks.vue"
 import Biography from "@/components/Biography.vue"
 import Project from "@/components/Project.vue"
 import ArtWork from "@/components/ArtGallery.vue"
+import Footer from "@/components/Footer.vue"
 
 export default {
     components: {
@@ -22,6 +24,7 @@ export default {
         "app-bio": Biography,
         "app-project": Project,
         "app-art": ArtWork,
+        "app-footer": Footer,
     },
 }
 </script>
