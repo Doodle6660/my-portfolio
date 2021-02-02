ee61e128f0428bf15c0880b19b6d57347ba94478
Doodle6660 <ziyadzidane69@gmail.com>
2021-02-02 12:06:40 +0700

    Update ArtGallery.vue

---
+++ src/components/ArtGallery.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex h-screen w-full hero">
+        <div class="w-full items-center">
+            <img src="@/assets/image/wave-art.svg" alt="" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.hero {
+    background: linear-gradient(
+            135deg,
+            rgba(32, 58, 67, 0.9) 0%,
+            rgba(15, 32, 39, 0.8) 100%
+        ),
+        url("./../assets/image/art-bg.jpg") center no-repeat;
+    background-size: cover;
+}
+</style>
