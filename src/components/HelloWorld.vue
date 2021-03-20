e8ffb9e7d72b45d110b3760fd9ef0a75bf22872b
Doodle6660 <ziyadzidane69@gmail.com>
2021-03-20 15:34:28 +0700

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,5 @@
 <template>
-    <div
-        class="flex flex-row h-screen bg-gradient-to-r from-black to-space text-white"
-    >
+    <div class="flex flex-row h-screen  text-white hello">
         <div
             class="flex flex-col max-w-xl h-full items-start justify-center ml-6 xl:ml-12 font-primary z-50"
         >
@@ -12,10 +10,10 @@
                 <p>Im Ziyad</p>
             </div>
             <div class="text-3xl xl:text-5xl mt-8">
-                <p>Front-End & UI/UX Designer</p>
+                <p class="p2">Front-End & UI/UX Designer</p>
             </div>
             <div class="text-md xl:text-2xl mt-32 md:mt-20 font-secondary z-50">
-                <p>
+                <p class="p3">
                     Hi!
                     <span class="font-semibold">I'm an 18-year-old maker</span>
                     who enjoys building simple & clean websites that are easy to
@@ -46,4 +44,21 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.hello {
+    background: url("./../assets/image/nature2.jpg") center no-repeat;
+    background-size: cover;
+}
+
+p {
+    text-shadow: 4px 4px rgba(0, 0, 0, 0.5);
+}
+
+.p2 {
+    text-shadow: 3px 3px rgba(0, 0, 0, 0.5);
+}
+
+.p3 {
+    text-shadow: 2px 2px rgba(0, 0, 0, 0.5);
+}
+</style>
